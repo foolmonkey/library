f6c84314db4fc249eaf91fc21d847f6142410eca
Andy Tran <foolmonkey99@gmail.com>
2020-06-05 13:39:39 -0600

    Refactor objects to be used with classes instead

--- script.js
+++ script.js
@@ -1,82 +1,94 @@
-let aLibrary = [];
-
-if (window.localStorage.length == 0) {
-    addBookToLibrary(aLibrary, "Harry Potter", "J.K. Rowling", 309);
-    addBookToLibrary(aLibrary, "The Fundamentals of Web Development", "Randy Connolly and Ricardo Hoar", 1184);
-    addBookToLibrary(aLibrary, "Dune", "Frank Herbert", 384, true);
-
-    window.localStorage.setItem("aLibrary", JSON.stringify(aLibrary));
-} else {
-    let aLibraryCopy = JSON.parse(window.localStorage.getItem("aLibrary"));
-
-    aLibrary = aLibraryCopy;
+class Book {
+    constructor(title, authorName, pages, readStatus = false) {
+        this.title = title;
+        this.author = authorName;
+        this.numOfPages = pages;
+        this.isRead = readStatus;
+    }
 }
 
-// constructor for book object
-function Book(title, authorName, pages, readStatus = false) {
-    this.title = title;
-    this.author = authorName;
-    this.numOfPages = pages;
-    this.isRead = readStatus;
-    this.index = aLibrary.length;
-}
+class Library {
+    constructor() {
+        this.books = [];
+    }
 
-// adds a book object to an array of books
-function addBookToLibrary(aLibrary, title, authorName, pages, readStatus = false) {
-    aLibrary.push(new Book(title, authorName, pages, readStatus));
+    get books() { return this._books; }
 
-    window.localStorage.setItem("aLibrary", JSON.stringify(aLibrary));
+    set books(value) { this._books = value; }
+
+    addBookToLibrary(title, authorName, pages, readStatus = false) {
+        this.books.push(new Book(title, authorName, pages, readStatus));
+
+        window.localStorage.setItem("library", JSON.stringify(this.books));
+    }
+
+    addExampleBooks() {
+        this.addBookToLibrary("Harry Potter", "J.K. Rowling", 309);
+        this.addBookToLibrary("The Fundamentals of Web Development", "Randy Connolly and Ricardo Hoar", 1184);
+        this.addBookToLibrary("Dune", "Frank Herbert", 384, true);
+    }
+
+    addLibraryToLocalStorage() {
+        if (window.localStorage.length == 0) {
+            window.localStorage.setItem("library", JSON.stringify(this.library));
+            this.addExampleBooks();
+        } else {
+            let libraryCopy = JSON.parse(window.localStorage.getItem("library"));
+
+            this.books = libraryCopy;
+        }
+    }
 }
 
 // takes an array of books and appends its values to a container
-function render(aLibrary) {
-    let container = document.getElementById('container');
+function render(library) {
+    let container = document.getElementById("container");
 
     container.innerHTML = "";
 
-    for (let i = 0, len = aLibrary.length; i < len; i++) {
-        let bookDisplay = document.createElement('div');
-        bookDisplay.classList.add('book');
+    for (let i = 0, len = library.length; i < len; i++) {
+        let bookDisplay = document.createElement("div");
+        bookDisplay.classList.add("book");
 
-        let bookInfo = document.createElement('div');
-        bookInfo.classList.add('bookInfo');
+        let bookInfo = document.createElement("div");
+        bookInfo.classList.add("bookInfo");
 
-        let title = document.createElement('h2');
-        title.classList.add('bookTitle');
-        title.textContent = aLibrary[i].title;
+        let title = document.createElement("h2");
+        title.classList.add("bookTitle");
+        title.textContent = library[i].title;
 
-        let author = document.createElement('p');
-        author.classList.add('author');
-        author.textContent = "By " + aLibrary[i].author;
+        let author = document.createElement("p");
+        author.classList.add("author");
+        author.textContent = "By " + library[i].author;
 
-        let numOfPages = document.createElement('p');
-        numOfPages.classList.add('numOfPages');
-        numOfPages.textContent = aLibrary[i].numOfPages + " pages";
+        let numOfPages = document.createElement("p");
+        numOfPages.classList.add("numOfPages");
+        numOfPages.textContent = library[i].numOfPages + " pages";
 
-        let isReadDiv = document.createElement('div');
-        isReadDiv.classList.add('isReadDiv');
+        let isReadDiv = document.createElement("div");
+        isReadDiv.classList.add("isReadDiv");
 
-        let isReadLabel = document.createElement('p');
-        isReadLabel.classList.add('isReadLabel');
-        isReadLabel.textContent = 'Has read:';
+        let isReadLabel = document.createElement("p");
+        isReadLabel.classList.add("isReadLabel");
+        isReadLabel.textContent = "Has read:";
 
-        let isRead = document.createElement('input')
-        isRead.classList.add('isRead');
-        isRead.setAttribute('type', 'checkbox');
-        if (aLibrary[i].isRead == true) {
-            isRead.setAttribute('checked', "");
+        let isRead = document.createElement("input");
+        isRead.classList.add("isRead");
+        isRead.setAttribute("type", "checkbox");
+        if (library[i].isRead == true) {
+            isRead.setAttribute("checked", "");
         }
 
-        let deleteButton = document.createElement('div');
+        let deleteButton = document.createElement("div");
         deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
-        deleteButton.classList.add('delete');
+        deleteButton.classList.add("delete");
 
-        deleteButton.addEventListener('click', function () {
-            aLibrary.splice(i, 1);
+        deleteButton.addEventListener("click", function () {
+            library.splice(i, 1);
 
             // update local storage
-            window.localStorage.setItem("aLibrary", JSON.stringify(aLibrary));
-            render(aLibrary);
+            window.localStorage.setItem("library", JSON.stringify(library));
+            render(library);
         });
 
         isReadDiv.appendChild(isReadLabel);
@@ -93,64 +105,72 @@ function render(aLibrary) {
 
         let readButton = document.getElementsByClassName("isRead")[i];
 
-        readButton.addEventListener('click', function () {
-            if (aLibrary[i].isRead == true) {
-                aLibrary[i].isRead = false;
+        readButton.addEventListener("click", function () {
+            if (library[i].isRead == true) {
+                library[i].isRead = false;
             } else {
-                aLibrary[i].isRead = true;
+                library[i].isRead = true;
             }
 
             // update local storage
-            window.localStorage.setItem("aLibrary", JSON.stringify(aLibrary));
+            window.localStorage.setItem("library", JSON.stringify(library));
         });
     }
 }
 
 window.onload = function (e) {
+    let library = new Library();
+    library.addLibraryToLocalStorage();
+
     // add books to library
-    render(aLibrary);
+    render(library.books);
 
     // add form functionality
-    let popup = document.getElementById('popup');
-    let inputFields = document.getElementsByClassName('bookData');
+    let popup = document.getElementById("popup");
+    let inputFields = document.getElementsByClassName("bookData");
 
     // add book button
-    let addButton = document.getElementById('addBook');
-    addButton.addEventListener('click', function (e) {
-        addButton.style.visibility = 'hidden';
-        popup.style.visibility = 'visible';
+    let addButton = document.getElementById("addBook");
+    addButton.addEventListener("click", function (e) {
+        addButton.style.visibility = "hidden";
+        popup.style.visibility = "visible";
     });
 
     // close window button
     let closeWindow = document.getElementById("cancel");
-    closeWindow.addEventListener('click', function (e) {
-        popup.style.visibility = 'hidden';
-        addButton.style.visibility = 'visible';
+    closeWindow.addEventListener("click", function (e) {
+        popup.style.visibility = "hidden";
+        addButton.style.visibility = "visible";
 
         // empty all the fields
         for (let i = 0; i < 3; i++) {
-            inputFields[i].value = ' ';
+            inputFields[i].value = "";
         }
     });
 
-    let submitButton = document.getElementById('add');
+    let submitButton = document.getElementById("add");
 
-    submitButton.addEventListener('click', function (e) {
+    submitButton.addEventListener("click", function (e) {
         let itemsFilled = true;
 
         for (let i = 0; i < 3; i++) {
-            if (inputFields[i].value.trim() == '') {
+            if (inputFields[i].value.trim() == "") {
                 itemsFilled = false;
             }
         }
 
         if (itemsFilled) {
-            addBookToLibrary(aLibrary, inputFields[0].value, inputFields[1].value, inputFields[2].value, inputFields[3].checked);
+            console.log('items filled');
+            library.addBookToLibrary(
+                inputFields[0].value,
+                inputFields[1].value,
+                inputFields[2].value,
+                inputFields[3].checked
+            );
 
-            popup.style.visibility = 'hidden';
-            addButton.style.visibility = 'visible';
-
-            render(aLibrary);
+            popup.style.visibility = "hidden";
+            addButton.style.visibility = "visible";
+            location.reload();
         }
     });
 };
